bc2201ea70131845e563f5ba587c1859bbc3ab3b
Mikko Jokinen <mikko.jokinen96@gmail.com>
2017-11-13 13:44:59 +0200

    Add files via upload

--- LadaGame/src/LadaGame.java
+++ LadaGame/src/LadaGame.java
@@ -1,18 +1,23 @@
+import java.io.File;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class LadaGame extends Application {
     Group god = new Group();
     Scene scene;
-    
+    public String lada = "lada.mp3";
+    Media hit = new Media(new File(lada).toURI().toString());
+    MediaPlayer mediaPlayer = new MediaPlayer(hit);
     @Override
     public void start(Stage primaryStage) {
-        
+        System.out.println(hit);
+        mediaPlayer.play();
         AnimationTimer timer;
         CreateMap createMap = new CreateMap();
         createMap.generateRoad();
@@ -21,11 +26,6 @@ public class LadaGame extends Application {
         scene = new Scene(god,createMap.sceneWidth,createMap.sceneHeight);
         scene.setFill(Color.GREEN);
         god.getChildren().addAll(createMap.groupForMap,createCar.stack);
-        
-        scene.setOnMouseMoved((MouseEvent e) -> {
-            createCar.move(e);
-        });
-        
         timer = new AnimationTimer() {
             @Override
             public void handle(long now) {

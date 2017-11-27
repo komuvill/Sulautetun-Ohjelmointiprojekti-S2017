c714c6c2378ec0d2cfa47d12413442903bca4bb0
komuvill <V-V.Komulainen@hotmail.com>
2017-11-27 10:04:27 +0200

    Kommentoitu

--- highscoreServer.py
+++ highscoreServer.py
@@ -3,11 +3,14 @@ import pymysql
 import netifaces #Käytetään Raspberryn IP:n löytämiseen dynaamisesti
 
 class highscoreServer:
+    #Luokan konstruktori
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.settimeout(None)
+        
+    #Avataan yhteys    
     def initialise(self):
         try:
             self.sock.bind((self.host, self.port))
@@ -16,14 +19,15 @@ class highscoreServer:
             return
         self.sock.listen(5)
         print("Server running on ",self.host,"and listening to ", self.port)
-
+    
+    #Pyöritetään tätä loputtomasti
     def serve_forever(self):
         try:
-            request, client_address = self.sock.accept()
-            print("Received connection from ", client_address)
+            request, client_address = self.sock.accept() #Odotellaan yhdistämistä clientiltä
+            print("Received connection from ", client_address) 
             data = request.recv(20)
-            stringdata = data.decode('utf-8', 'ignore')
-            stringdata = stringdata[2:len(stringdata)].split(",")
+            stringdata = data.decode('utf-8', 'ignore') #Data saapuu UTF-8-koodattuna
+            stringdata = stringdata[2:len(stringdata)].split(",") #Siivotaan alun "roska"merkit ja palastellaan data nimeen ja pisteisiin
             name = stringdata[0]
             score = int(stringdata[1])
             print("Received data: ", name, " with the score of ", score)
@@ -35,6 +39,7 @@ class highscoreServer:
             request.close()
     @staticmethod
     def insertHighScores(name, score):
+        # Yhdistetään tietokantaan ja lisätään tiedot
         query = "INSERT INTO leaderboards(name, points) VALUES('{0}',{1})".format(name, score)
         db = pymysql.connect(host="mysli.oamk.fi", user="t6kovi01", passwd="", db="opisk_t6kovi01")
         cur = db.cursor()
@@ -42,13 +47,15 @@ class highscoreServer:
         db.commit()
         db.close()
         
+# Määritellään skriptin pääfunktio        
 def main():
     netifaces.ifaddresses("wlan0")
     host = netifaces.ifaddresses("wlan0")[2][0]["addr"] #Haetaan raspberryn IP
-    server = highscoreServer(host, 20000)
+    server = highscoreServer(host, 20000) # luodaan serveri
     server.initialise()
     while True:
         server.serve_forever()
-
+        
+#Ajetaan pääfunktio
 if __name__ == "__main__":
     main()

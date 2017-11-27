3b05040fc26db21da56d0ad6748a2f04650b4850
komuvill <V-V.Komulainen@hotmail.com>
2017-11-27 10:13:45 +0200

    Kommentoitu

--- LadaGame/src/HighscoreClient.java
+++ LadaGame/src/HighscoreClient.java
@@ -14,7 +14,7 @@ public class HighscoreClient extends JFrame{
     private String nimi;
     private int pisteet;
     private String dataString;
-    private final Socket clientSocket;
+    private final Socket clientSocket; 
     private final JButton submitButton;
     private final JLabel scoreLabel;
     private final JLabel nameLabel;
@@ -22,8 +22,9 @@ public class HighscoreClient extends JFrame{
     private final JPanel panel;
     private final DataOutputStream toServer;
     
+    //Luokan konstruktori ottaa parametrina pelaajan pistemäärän
     public HighscoreClient(int pelaajanPisteet) throws IOException{
-
+        // Alustetaan labelit yms.
         this.setSize(250,250);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,7 +36,7 @@ public class HighscoreClient extends JFrame{
         nameField = new JTextField(25);
         submitButton = new JButton("Submit score!");
         panel = new JPanel(new GridBagLayout());
-        
+        //Lisätään elementit ikkunaan
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -49,10 +50,10 @@ public class HighscoreClient extends JFrame{
         gbc.gridy = 1;
         gbc.gridx = 1;
         panel.add(nameField,gbc);
-
+        //Lisätään pisteiden lähettämisnapille tapahtumakuuntelija
         ListenForButton submitClicked = new ListenForButton();
         submitButton.addActionListener(submitClicked);
-        
+        //Luodaan serveriin yhdistämiseen tarvittavat socketit ja datastreamit
         clientSocket = new Socket("10.4.3.6", 20000); //Tähän Raspberryn IP
         toServer = new DataOutputStream(clientSocket.getOutputStream());
         
@@ -68,10 +69,11 @@ public class HighscoreClient extends JFrame{
         @Override
         public void actionPerformed(ActionEvent ae) {
             if(ae.getSource() == submitButton){
+                //Parsitaan lähetettävä data
                 nimi = nameField.getText();
                 dataString = nimi + "," + Integer.toString(pisteet);
                 try {
-                    toServer.writeUTF(dataString);
+                    toServer.writeUTF(dataString); //Lähetetään data
                 } catch (IOException ex) {
                     Logger.getLogger(HighscoreClient.class.getName()).log(Level.SEVERE, null, ex);
                 }
